e06d2524a9be585ff455ca63825d245e7515be9e
rzarifov <i@rzarifov.ru>
2024-08-08 15:16:30 +0300

    task2_completed

---
+++ src/Main2.java
@@ -0,0 +1,19 @@
+//public class Main2 {
+//    public static void main(String[] args) {
+//        for (int i = 2; i < 101;) {
+//            System.out.println("Уволен сотрудник с id=" + i);
+//            i+=2;
+//        }
+//    }
+//}
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i < 100) {
+            System.out.println("Уволен сотрудник с id=" + i);
+            i+=2;
+        }
+    }
+}
